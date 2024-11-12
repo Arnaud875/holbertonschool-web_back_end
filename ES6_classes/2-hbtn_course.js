13537f2dec78f83912cdf9c1596f6bfb29e4062b
Arnaud875 <171556021+Arnaud875@users.noreply.github.com>
2024-11-12 17:42:16 +0100

    Fix check arrays

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -32,7 +32,7 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
-    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+    if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array');
     }
     this._students = newStudents;
--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -35,6 +35,6 @@ export default class Currency {
   }
 
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+    return `${this.name} (${this.code})`;
   }
 }
