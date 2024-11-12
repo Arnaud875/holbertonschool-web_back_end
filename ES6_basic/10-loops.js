b02279466eaea28386bf01175d470a9b84b94ee8
Arnaud875 <171556021+Arnaud875@users.noreply.github.com>
2024-11-12 16:09:39 +0100

    Fix eslint format

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    let i = 0;
-    for (const value of array) {
-        array[i] = appendString + value;
-        i++;
-    }
+  let i = 0;
+  for (const value of array) {
+    array[i] = appendString + value;
+    i++;
+  }
 
-    return array;
+  return array;
 }
