6596ff0bb46f6da1404b767634325e70febca060
Arnaud875 <arnaud.nickles33@gmail.com>
2024-11-13 11:44:12 +0100

    remove getter and setter

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -4,28 +4,6 @@ export default class Airport {
     this._code = code;
   }
 
-  get name() {
-    return this._name;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set name(newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = newName;
-  }
-
-  set code(newCode) {
-    if (typeof newCode !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    this._code = newCode;
-  }
-
   toString() {
     return `[object ${this._code}]`;
   }
