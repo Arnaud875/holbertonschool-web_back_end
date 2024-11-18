cf5561d32e2053efeb2adde1a0e817b1788e1c08
Arnaud875 <arnaud.nickles33@gmail.com>
2024-11-18 10:27:35 +0100

    Modify slice by replace function and add throw error

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,10 @@ export default function cleanSet(setObj, startString) {
   const substrings = [];
 
   setObj.forEach((value) => {
-    if (typeof value === 'string' && startString !== '' && value.startsWith(startString)) substrings.push(value.slice(startString.length));
+    if (typeof value === 'string' && startString !== '' && value.startsWith(startString)) substrings.push(value.replace(startString, ''));
   });
   return substrings.join('-');
 }
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
