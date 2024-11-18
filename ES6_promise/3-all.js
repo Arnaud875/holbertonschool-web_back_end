afd79147b0e9baf971231c812ca3ceb2fb36e6e3
Arnaud875 <arnaud.nickles33@gmail.com>
2024-11-18 12:18:44 +0100

    Fix error message

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  createUser().then((user) => {
-    uploadPhoto().then((response) => {
+  Promise.all([createUser(), uploadPhoto()])
+    .then(([user, response]) => {
       console.log(response.body, user.firstName, user.lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
     });
-  });
 }
