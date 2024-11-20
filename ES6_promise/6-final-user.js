a29e7297338bf1a97558e8bb1a48647e39474b45
Arnaud875 <arnaud.nickles33@gmail.com>
2024-11-20 09:59:08 +0100

    Fix task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return (await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]))
+    .map((res) => ({ status: res.status, value: res.status === 'fulfilled' ? res.value : `Error: ${res.reason.message}` }));
 }
