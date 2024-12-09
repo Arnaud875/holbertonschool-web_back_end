6cc7f1cf2a19144405071cb878f342ece13a94f9
Arnaud875 <arnaud.nickles33@gmail.com>
2024-12-09 10:49:30 +0100

    Fix task 3

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -39,7 +39,7 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(data) / page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
 
         return {
             "page_size": len(data),
--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -46,48 +46,18 @@ class Server:
         next_index = index
 
         for _ in range(page_size):
-            while next_index not in indexed_data and next_index < len(self.dataset()):
-                next_index += 1
+            while next_index not in indexed_data:
+                if next_index < len(self.dataset()):
+                    next_index += 1
             if next_index >= len(self.dataset()):
                 break
             data.append(indexed_data[next_index])
             next_index += 1
 
+        n = next_index if next_index < len(self.dataset()) else None
         return {
             "index": index,
             "page_size": page_size,
             "data": data,
-            "next_index": next_index if next_index < len(self.dataset()) else None,
+            "next_index": n,
         }
-
-server = Server()
-
-server.indexed_dataset()
-
-try:
-    server.get_hyper_index(300000, 100)
-except AssertionError:
-    print("AssertionError raised when out of range")        
-
-
-index = 3
-page_size = 2
-
-print("Nb items: {}".format(len(server._Server__indexed_dataset)))
-
-# 1- request first index
-res = server.get_hyper_index(index, page_size)
-print(res)
-
-# 2- request next index
-print(server.get_hyper_index(res.get('next_index'), page_size))
-
-# 3- remove the first index
-del server._Server__indexed_dataset[res.get('index')]
-print("Nb items: {}".format(len(server._Server__indexed_dataset)))
-
-# 4- request again the initial index -> the first data retreives is not the same as the first request
-print(server.get_hyper_index(index, page_size))
-
-# 5- request again initial next index -> same data page as the request 2-
-print(server.get_hyper_index(res.get('next_index'), page_size))
