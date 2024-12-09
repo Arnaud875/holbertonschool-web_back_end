c9d49949438cf51508ede04846b4392a015af64b
Arnaud875 <arnaud.nickles33@gmail.com>
2024-12-09 10:16:47 +0100

    Project 0

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Asynchronous comprehension example."""
+
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[int]:
+    """Collect 10 random numbers using an asynchronous comprehension."""
+    return [number async for number in async_generator()]
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Asynchronous comprehension example."""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measure the runtime of async_comprehension."""
+    start = time.time()
+    await async_comprehension()
+    end = time.time()
+    return end - start
