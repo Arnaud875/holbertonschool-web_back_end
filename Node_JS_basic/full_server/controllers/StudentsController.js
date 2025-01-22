924bba9eae1bc8a8f70439e88b79863f3efbb7ea
Arnaud875 <arnaud.nickles33@gmail.com>
2025-01-22 09:57:13 +0100

    lint

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,42 +1,42 @@
-import utils from "../utils";
+import utils from '../utils';
 
 export default class StudentsController {
-    static async getAllStudents(request, response) {
-        utils()
-            .then(data => {
-                data.CS = data.CS.map(el => el.firstname);
-                data.SWE = data.SWE.map(el => el.firstname);
+  static async getAllStudents(request, response) {
+    utils()
+      .then((data) => {
+        const _data = data;
+        _data.CS = _data.CS.map((el) => el.firstname);
+        _data.SWE = _data.SWE.map((el) => el.firstname);
 
-                let formatStr = 'This is the list of our students\n';
-                formatStr += `Number of students in CS: ${data.CS.length}. List: ${data.CS.join(', ')}\n`;
-                formatStr += `Number of students in SWE: ${data.SWE.length}. List: ${data.SWE.join(', ')}`;
+        let formatStr = 'This is the list of our students\n';
+        formatStr += `Number of students in CS: ${_data.CS.length}. List: ${_data.CS.join(', ')}\n`;
+        formatStr += `Number of students in SWE: ${_data.SWE.length}. List: ${_data.SWE.join(', ')}`;
 
-                response.status(200).send(formatStr);
-            })
-            .catch(() => {
-                response.status(500).send("Cannot load database");
-            });
+        response.status(200).send(formatStr);
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load database');
+      });
+  }
+
+  static async getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
+
+    if (!['CS', 'SWE'].includes(major)) {
+      response.status(500).send('Major parameter must be CS or SWE');
+    } else {
+      utils()
+        .then((data) => {
+          let str;
+
+          if (major === 'CS') str = data.CS.map((el) => el.firstname).join(', ');
+          else str = data.SWE.map((el) => el.firstname).join(', ');
+
+          response.status(200).send(`List: ${str}`);
+        })
+        .catch(() => {
+          response.status(500).send('Cannot load database');
+        });
     }
-
-    static async getAllStudentsByMajor(request, response) {
-        const major = request.params.major;
-
-        if (!["CS", "SWE"].includes(major))
-            return response.status(500).send("Major parameter must be CS or SWE");
-
-        utils()
-            .then(data => {
-                let str;
-
-                if (major == "CS")
-                    str = data.CS.map(el => el.firstname).join(", ");
-                else
-                    str = data.SWE.map(el => el.firstname).join(", ");
-
-                response.status(200).send(`List: ${str}`);
-            })
-            .catch(() => {
-                response.status(500).send("Cannot load database");
-            });
-    }
+  }
 }
--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,31 +1,30 @@
-import { existsSync, readFileSync } from "fs";
+import { existsSync, readFileSync } from 'fs';
 
 export default async function readDatabase() {
-    return new Promise((resolve, reject) => {
-        const filePath = (process.argv.length > 2) ? process.argv[2] : "";
+  return new Promise((resolve, reject) => {
+    const filePath = (process.argv.length > 2) ? process.argv[2] : '';
 
-        if (!existsSync(filePath)) reject(new Error('Cannot load the database'));
-        
-        let data = readFileSync(filePath, { encoding: 'utf-8' });
-        data = data.split('\n').map((line) => line.split(','));
-    
-        const rowsName = data[0];
-    
-        let table = { "size": 0, "SWE": [], "CS": [] }
-    
-        for (const elements of data.slice(1)) {
-            const formatElements = {};
-    
-            elements.forEach((element, index) => {
-                formatElements[rowsName[index]] = element;
-            });
+    if (!existsSync(filePath)) reject(new Error('Cannot load the database'));
 
-            if (typeof(formatElements.field) != "undefined")
-                table[formatElements.field].push(formatElements);
+    let data = readFileSync(filePath, { encoding: 'utf-8' });
+    data = data.split('\n').map((line) => line.split(','));
 
-            if (elements[0] !== '') table.size += 1;
-        }
-    
-        resolve(table);
-    });
+    const rowsName = data[0];
+
+    const table = { size: 0, SWE: [], CS: [] };
+
+    for (const elements of data.slice(1)) {
+      const formatElements = {};
+
+      elements.forEach((element, index) => {
+        formatElements[rowsName[index]] = element;
+      });
+
+      if (typeof (formatElements.field) !== 'undefined') table[formatElements.field].push(formatElements);
+
+      if (elements[0] !== '') table.size += 1;
+    }
+
+    resolve(table);
+  });
 }
